e0fce922283b33e03b1c1f88ec42cf8aed7830f5
Tanabe <tanabe2852@yahoo.co.jp>
2021-03-13 17:36:14 +0900

    右モーターの上限を加える

--- main.ts
+++ main.ts
@@ -1,19 +1,27 @@
+let 右スピード = 0
 function 右モーター (スピード: number) {
-    if (スピード >= 0) {
+    右スピード = スピード
+    if (右スピード >= 0) {
         pins.digitalWritePin(DigitalPin.P15, 0)
         pins.digitalWritePin(DigitalPin.P14, 1)
-        pins.analogWritePin(AnalogPin.P13, スピード)
     } else {
         pins.digitalWritePin(DigitalPin.P15, 1)
         pins.digitalWritePin(DigitalPin.P14, 0)
-        pins.analogWritePin(AnalogPin.P13, スピード * -1)
     }
+    if (右スピード > 1024) {
+        右スピード = 1023
+    }
+    if (右スピード < -1023) {
+        右スピード = -1023
+    }
+    pins.analogWritePin(AnalogPin.P13, Math.abs(右スピード))
 }
 basic.forever(function () {
     basic.showIcon(IconNames.Heart)
     serial.writeValue("0", pins.analogReadPin(AnalogPin.P0))
     serial.writeValue("1", pins.analogReadPin(AnalogPin.P1))
     serial.writeValue("2", pins.analogReadPin(AnalogPin.P2))
+    serial.writeValue("line_pos", pins.analogReadPin(AnalogPin.P2) - pins.analogReadPin(AnalogPin.P1))
     serial.writeLine("-------------------------------------------------")
     右モーター(0)
 })

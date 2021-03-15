88c81ee9fe73a5d1f85266bfea27e17b89669f87
Tanabe <tanabe2852@yahoo.co.jp>
2021-03-15 18:07:46 +0900

    左モーター追加

--- main.ts
+++ main.ts
@@ -1,8 +1,26 @@
 let 差分 = 0
+let 左スピード = 0
 let 右スピード = 0
 function ライン位置 () {
     差分 = pins.analogReadPin(AnalogPin.P2) - pins.analogReadPin(AnalogPin.P1)
     return 差分
+}
+function 左モーター (スピード: number) {
+    左スピード = スピード
+    if (左スピード >= 0) {
+        pins.digitalWritePin(DigitalPin.P12, 0)
+        pins.digitalWritePin(DigitalPin.P16, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P12, 1)
+        pins.digitalWritePin(DigitalPin.P16, 0)
+    }
+    if (左スピード > 1024) {
+        左スピード = 1023
+    }
+    if (左スピード < -1023) {
+        左スピード = -1023
+    }
+    pins.analogWritePin(AnalogPin.P8, Math.abs(左スピード))
 }
 function 右モーター (スピード: number) {
     右スピード = スピード
@@ -28,5 +46,7 @@ basic.forever(function () {
     serial.writeValue("2", pins.analogReadPin(AnalogPin.P2))
     serial.writeValue("line_pos", ライン位置())
     serial.writeLine("-------------------------------------------------")
-    右モーター(0)
+    右モーター(2000)
+    左モーター(2000)
+    ライン位置()
 })

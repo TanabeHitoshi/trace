6536087a832628232188f9d1bf56b0e4a6e1e18d
Tanabe <tanabe2852@yahoo.co.jp>
2021-06-26 14:28:51 +0900

    右手で停止する

--- main.py
+++ main.py
@@ -29,6 +29,8 @@ def 走る(スピード: number, 回転速度: number):
 def on_button_pressed_b():
     global pattern
     basic.clear_screen()
+    右モーター(0)
+    左モーター(0)
     pattern = 20
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
@@ -74,17 +76,17 @@ def 右モーター(スピード: number):
         pins.digital_write_pin(DigitalPin.P15, 1)
     else:
         pins.analog_write_pin(AnalogPin.P13, abs(右スピード))
-def 右手2():
-    pass
 def 右手():
-    global kp, kd, 差分, 微分, 制御量, 前差分
-    kp = 20
-    kd = 20
-    差分 = pins.analog_read_pin(AnalogPin.P1) - pins.analog_read_pin(AnalogPin.P2)
-    微分 = 差分 - 前差分
-    制御量 = 差分 * kp + 微分 * kd
-    前差分 = 差分
-    return 制御量
+    global state, 右手回数
+    if pins.analog_read_pin(AnalogPin.P0) < 500:
+        basic.pause(10)
+        if pins.analog_read_pin(AnalogPin.P0) < 500:
+            state = "on"
+    if pins.analog_read_pin(AnalogPin.P0) > 600 and state == "on":
+        右手回数 += 1
+        state = "off"
+    return 右手回数
+state = ""
 右スピード = 0
 左スピード = 0
 制御量 = 0
@@ -93,6 +95,7 @@ def 右手():
 差分 = 0
 kd = 0
 kp = 0
+右手回数 = 0
 pattern = 0
 basic.show_leds("""
     . # # # .
@@ -104,11 +107,22 @@ basic.show_leds("""
 右モーター(0)
 左モーター(0)
 pattern = 0
+右手回数 = 0
 
 def on_forever():
+    global pattern
     if pattern == 10:
+        if 右手回数 == 3:
+            pattern = 100
+        右手()
         走る(700, ライン位置())
     if pattern == 20:
+        serial.write_value("n", 右手())
         モニタ()
         basic.pause(500)
+    if pattern == 30:
+        serial.write_value("n", 右手())
+    if pattern == 100:
+        右モーター(0)
+        左モーター(0)
 basic.forever(on_forever)
